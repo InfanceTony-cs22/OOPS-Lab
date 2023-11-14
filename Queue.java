e3dbb5d69b2603e418c63aef009848b893f0d010
Infance Tony.E <138364215+InfanceTony-cs22@users.noreply.github.com>
2023-11-14 21:19:17 +0530

    Create Queue.java

---
+++ Queue.java
@@ -0,0 +1,46 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class CustomQueue<T> {
+    private Queue<T> queue;
+
+    public CustomQueue() {
+        this.queue = new LinkedList<>();
+    }
+
+    public void enqueue(T element) {
+        queue.add(element);
+    }
+
+    public T dequeue() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Queue is empty");
+        }
+        return queue.poll();
+    }
+
+    public T peek() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Queue is empty");
+        }
+        return queue.peek();
+    }
+
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
+
+    public int size() {
+        return queue.size();
+    }
+}
+
+public class QueueExample {
+    public static void main(String[] args) {
+        CustomQueue<Integer> customQueue = new CustomQueue<>();
+
+        customQueue.enqueue(10);
+        customQueue.enqueue(20);
+        customQueue.enqueue(30);
+
+        System.out.println("Queue size: " + customQueue.size
